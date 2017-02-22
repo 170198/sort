ae53d0b1775236b1c2f9355b367acae034078bb8
170198 <benmowat@me.com>
2017-02-22 09:05:31 +0800

    insertion
    added insertion

--- src/sort/SortOutYourLife.java
+++ src/sort/SortOutYourLife.java
@@ -2,10 +2,14 @@ package sort;
 
 public class SortOutYourLife {
 	public static void main(String[]args){
-		int[]x={9,8,7,6,5,4,3,2,1,0};
+		int[]x={2,3,6,7,4,8,5,1,0,9};
 		//int[]x={0,1,2,3,4,5,6,7,8,9};
 		int[]y=new int[10];
 		y=sorting.selection(x);
+		for(int r = 0; r< y.length;r++){
+			System.out.println(y[r]);
+		}
+		y=sorting.insertion(x);
 		for(int r = 0; r< y.length;r++){
 			System.out.println(y[r]);
 		}
--- src/sort/sorting.java
+++ src/sort/sorting.java
@@ -10,21 +10,36 @@ public class sorting {
 		//for(int r = 0; r<arr2.length;r++){
 			//System.out.println(arr2[r]);
 		//}
-		for(int e = 0;e<length;e++){
+		for(int e = 0;e<length;e++){ //allows for all of the array to be sorted no matter how long
 			for(int x = 0;x<length-1;x++){
 			if(arr2[x]>arr2[x+1]){
-				int z = arr2[x];
+				int f = arr2[x]; //assigns it to z in order to for the values to be switched without losing the original value
 				arr2[x]=arr2[x+1];
-				arr2[x+1]=z;
-				
-			}
-			
+				arr2[x+1]=f;				
+			}			
+		}		
 		}
-		
-		}
-		return arr2;
+		return arr2;	
+	}
 	
+	
+	public static int[] insertion(int [] arr1){
+		int length = arr1.length;
+		int[] arr2 = new int[length];
+		for(int z =0; z<length; z++){
+			arr2[z]=arr1[z];
+		}
 		
+		for(int x = 0;x<length;x++){
+			for(int z = 0;z<length-1;z++){	
+				if(arr2[x]<arr2[z]){
+					int e = arr2[x];
+					arr2[x]=arr2[z];
+					arr2[z]=e;
+					}
+				}
+			}		
+		return arr2;
 	}
 
 }
